5d739122fbcd092d7872b3676928a47b04c067b7
Jake <user@FIH-COM23a111.fsu.edu>
2026-04-22 16:16:05 -0400

    added ai enemy scripts

--- Assets/Gabe Folder/spawnController.cs
+++ Assets/Gabe Folder/spawnController.cs
@@ -1,19 +1,64 @@
 using UnityEngine;
+using System.Collections;
 
-public class spawnController : MonoBehaviour
+public class SpawnController : MonoBehaviour
 {
+    [Header("gates")]
     public GateController Gate;
     public GateController largeGate;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Spawn Settings")]
+    public GameObject enemyPrefab;
+    public Transform[] spawnPoints;
+
+    [Header("Limits")]
+    public int maxEnemies = 10;
+    public int spawnBatchSize = 3;
+
+    [Header("Timing")]
+    public float checkInterval = 5f;
+
+    private int currentEnemyCount = 0;
+
     void Start()
     {
-
+        StartCoroutine(SpawnLoop());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SpawnLoop()
     {
+        while (true)
+        {
+            yield return new WaitForSeconds(checkInterval);
 
+            // Check if we can spawn more enemies
+            if (currentEnemyCount < maxEnemies)
+            {
+                SpawnEnemies();
+            }
+        }
     }
-   
+
+    void SpawnEnemies()
+    {
+        int spawnAmount = Mathf.Min(spawnBatchSize, maxEnemies - currentEnemyCount);
+
+        for (int i = 0; i < spawnAmount; i++)
+        {
+            Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+
+            GameObject enemy = Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+
+            currentEnemyCount++;
+
+            // Hook into enemy death so we can track removal
+            EnemyTracker tracker = enemy.AddComponent<EnemyTracker>();
+            tracker.SetSpawner(this);
+        }
+    }
+
+    // Called by enemies when they die
+    public void OnEnemyDeath()
+    {
+        currentEnemyCount = Mathf.Max(0, currentEnemyCount - 1);
+    }
 }
